50442e31fc5cb46398c9b6accb312abfebb800b5
Ryan O'Meara <romeara@live.com>
2017-10-16 22:23:17 -0400

    Disambiguate app layer from project name

--- main.webapp/src/main/java/org/starchartlabs/chronicler/main/webapp/app/model/RootView.java
+++ main.webapp/src/main/java/org/starchartlabs/chronicler/main/webapp/app/model/RootView.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.starchartlabs.chronicler.main.app;
+package org.starchartlabs.chronicler.main.webapp.app.model;
 
 import java.util.Objects;
 
