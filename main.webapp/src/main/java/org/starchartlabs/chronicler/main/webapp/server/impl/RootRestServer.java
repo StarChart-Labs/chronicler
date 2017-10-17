f5b144bd9adf843b7ba26485512f216f56b6ef6b
Nicholas Ver Voort <moldorma@gmail.com>
2017-10-16 23:24:55 -0400

    Initial setup for homepage development

--- main.webapp/src/main/java/org/starchartlabs/chronicler/main/webapp/server/impl/RootRestServer.java
+++ main.webapp/src/main/java/org/starchartlabs/chronicler/main/webapp/server/impl/RootRestServer.java
@@ -20,6 +20,7 @@ import java.util.Objects;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -44,6 +45,18 @@ public class RootRestServer {
         RootView rootView = new RootView(installationUrl, sourceUrl);
 
         return new ResponseEntity<>(rootView, HttpStatus.OK);
+    }
+    
+    /**
+     * Renders the homepage
+     * @param model The model to be rendered into a webpage
+     * @return The name of the template to render
+     */
+    @RequestMapping(method = RequestMethod.GET, path = "/", produces = { "text/html" })
+    public String getRoot(Model model) {
+        model.addAttribute("rootView", new RootView(installationUrl, sourceUrl));
+
+        return "index";
     }
 
 }
